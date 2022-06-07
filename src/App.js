77fb1be7d7424d1e62ea7b88e50f85d3217a8e8e
Alok4k45h <alok4k45h@gmail.com>
2022-06-08 01:42:43 +0530

    initial code

---
+++ src/App.js
@@ -0,0 +1,38 @@
+// importing required packages
+import { useState } from "react";
+import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
+
+// importing required Components
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import Navbar from "./components/Navbar";
+import Add from "./components/Add";
+
+function App() {
+  // state variable for dark && Light mode
+  const [mode, setMode] = useState("light");
+
+  // creating manual theme for dark and light mode
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar setMode={setMode} mode={mode} />
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default App;
